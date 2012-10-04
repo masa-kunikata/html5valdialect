aedcdb170745f22dad2674091fd64b63585f2f84
Jose Samper <jmiguelsamper@users.sourceforge.net>
2012-10-04 09:24:43 +0000

    Cleaning test code

--- src/test/java/unit/net/sourceforge/html5val/performers/URLRegexpComposerTest.java
+++ src/test/java/unit/net/sourceforge/html5val/performers/URLRegexpComposerTest.java
@@ -3,40 +3,22 @@ package unit.net.sourceforge.html5val.performers;
 import java.util.regex.Pattern;
 import net.sourceforge.html5val.performers.URLRegexpComposer;
 import org.hibernate.validator.constraints.URL;
-import org.jmock.Expectations;
-import org.jmock.Mockery;
-import org.jmock.integration.junit4.JUnit4Mockery;
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class URLRegexpComposerTest {
 
-    private final Mockery context = new JUnit4Mockery();
-    private URL urlAnnotation;
     private static final String REGEXP = "regexp";
-
-    @Before
-    public void createMock() {
-        urlAnnotation = context.mock(URL.class);
-    }
 
     @Test
     public void annotationWithPattern() {
-        context.checking(new Expectations(){{
-            allowing(urlAnnotation).regexp(); will(returnValue(REGEXP));
-        }});
+        URL urlAnnotation = new MockURLBuilder().withRegexp(REGEXP).build();
         assertEquals(REGEXP, URLRegexpComposer.forURL(urlAnnotation).regexp());
      }
 
     @Test
     public void annotationWithProtocol() {
-        context.checking(new Expectations(){{
-            allowing(urlAnnotation).regexp(); will(returnValue(URLRegexpComposer.DEFAULT_REGEXP));
-            allowing(urlAnnotation).protocol(); will(returnValue("http"));
-            allowing(urlAnnotation).port(); will(returnValue(URLRegexpComposer.EMPTY_PORT));
-            allowing(urlAnnotation);
-        }});
+        URL urlAnnotation = new MockURLBuilder().withProtocol("http").build();
         String pattern = "^http://.+(:[0-9]+)?(/.*)?";
         checkRegexpNotMatches(pattern, "://www.ya.com/");
         checkRegexpNotMatches(pattern, "ftp://www.ya.com/");
@@ -49,12 +31,7 @@ public class URLRegexpComposerTest {
 
     @Test
     public void annotationWithHost() {
-        context.checking(new Expectations(){{
-            allowing(urlAnnotation).regexp(); will(returnValue(URLRegexpComposer.DEFAULT_REGEXP));
-            allowing(urlAnnotation).host(); will(returnValue("www.ya.com"));
-            allowing(urlAnnotation).port(); will(returnValue(URLRegexpComposer.EMPTY_PORT));
-            allowing(urlAnnotation);
-        }});
+        URL urlAnnotation = new MockURLBuilder().withHost("www.ya.com").build();
         String pattern = "^.+://www.ya.com(:[0-9]+)?(/.*)?";
         checkRegexpNotMatches(pattern, "://www.ya.com/");
         checkRegexpNotMatches(pattern, "http://www.google.com:443/");
@@ -71,11 +48,7 @@ public class URLRegexpComposerTest {
 
     @Test
     public void annotationWithPort() {
-        context.checking(new Expectations(){{
-            allowing(urlAnnotation).regexp(); will(returnValue(URLRegexpComposer.DEFAULT_REGEXP));
-            allowing(urlAnnotation).port(); will(returnValue(443));
-            allowing(urlAnnotation);
-        }});
+        URL urlAnnotation = new MockURLBuilder().withPort(443).build();
         String pattern = "^.+://.+:443(/.*)?";
         checkRegexpNotMatches(pattern, "://www.google.com:443/");
         checkRegexpMatches(pattern, "http://www.ya.com:443");
@@ -90,12 +63,7 @@ public class URLRegexpComposerTest {
 
     @Test
     public void annotationWithProtocolHostAndPort() {
-        context.checking(new Expectations(){{
-            allowing(urlAnnotation).regexp(); will(returnValue(URLRegexpComposer.DEFAULT_REGEXP));
-            allowing(urlAnnotation).protocol(); will(returnValue("http"));
-            allowing(urlAnnotation).host(); will(returnValue("www.google.com"));
-            allowing(urlAnnotation).port(); will(returnValue(8080));
-        }});
+        URL urlAnnotation = new MockURLBuilder().withProtocol("http").withHost("www.google.com").withPort(8080).build();
         String pattern = "^http://www.google.com:8080(/.*)?";
         checkRegexpMatches(pattern, "http://www.google.com:8080/");
         checkRegexpNotMatches(pattern, "http://www.google.com:8080bill.html");
