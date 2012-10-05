bd85b6e3eda89b3737e2e3cd72120e30877b5301
Jose Samper <jmiguelsamper@users.sourceforge.net>
2012-10-05 09:53:55 +0000

    ValidationChecker for several Constraints

--- src/test/java/unit/net/sourceforge/html5val/performers/ValidationChecker.java
+++ src/test/java/unit/net/sourceforge/html5val/performers/ValidationChecker.java
@@ -1,7 +1,15 @@
 package unit.net.sourceforge.html5val.performers;
 
+import java.lang.annotation.Annotation;
+import javax.validation.ConstraintValidator;
 import javax.validation.constraints.Digits;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
 import org.hibernate.validator.internal.constraintvalidators.DigitsValidatorForCharSequence;
+import org.hibernate.validator.internal.constraintvalidators.LengthValidator;
+import org.hibernate.validator.internal.constraintvalidators.SizeValidatorForCharSequence;
+import org.hibernate.validator.internal.constraintvalidators.URLValidator;
 import static org.junit.Assert.*;
 import static unit.util.RegexpMatcher.matchesRegexp;
 import static unit.util.RegexpMismatcher.doesNotMatchRegexp;
@@ -11,24 +19,38 @@ import static unit.util.RegexpMismatcher.doesNotMatchRegexp;
  */
 class ValidationChecker {
 
-    private DigitsValidatorForCharSequence validator;
+    private ConstraintValidator validator;
     private String pattern;
-    private Digits digitsAnnotation;
+    private Annotation annotation;
 
-    public ValidationChecker(Digits digitsAnnotation, String pattern) {
+    public ValidationChecker(Annotation annotation, String pattern) {
         this.pattern = pattern;
-        this.digitsAnnotation = digitsAnnotation;
-        validator = new DigitsValidatorForCharSequence();
-        validator.initialize(digitsAnnotation);
+        this.annotation = annotation;
+        validator = validatorFor(annotation);
+        validator.initialize(annotation);
+    }
+
+    private ConstraintValidator validatorFor(Annotation annotation) {
+        if (annotation instanceof Digits) {
+            return new DigitsValidatorForCharSequence();
+        } else if (annotation instanceof URL) {
+            return new URLValidator();
+        } else if (annotation instanceof Length) {
+            return new LengthValidator();
+        } else if (annotation instanceof Size) {
+            return new SizeValidatorForCharSequence();
+        } else {
+            throw new IllegalArgumentException("ConstraintValidator unknown");
+        }
     }
 
     public void isValid(String value) {
         assertThat(value, matchesRegexp(pattern));
-        assertTrue(String.format("%s is not valid for @Digits(%s, %s)", value, digitsAnnotation.integer(), digitsAnnotation.fraction()), validator.isValid(value, null));
+        assertTrue(String.format("%s is not valid for annotation %s", value, annotation.toString()), validator.isValid(value, null));
     }
 
     public void isNotValid(String value) {
         assertThat(value, doesNotMatchRegexp(pattern));
-        assertFalse(String.format("%s should not be valid for @Digits(%s, %s)", value, digitsAnnotation.integer(), digitsAnnotation.fraction()), validator.isValid(value, null));
+        assertFalse(String.format("%s should not be valid for annotation %s", value, annotation.toString()), validator.isValid(value, null));
     }
 }
