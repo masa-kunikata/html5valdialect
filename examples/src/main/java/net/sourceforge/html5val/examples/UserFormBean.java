c159e17c8588164c6eb10eb09b52f009d701df08
jacksonbeat <jacksonbeat>
2013-02-01 08:36:28 +0000

    Import

--- examples/src/main/java/net/sourceforge/html5val/examples/UserFormBean.java
+++ examples/src/main/java/net/sourceforge/html5val/examples/UserFormBean.java
@@ -2,6 +2,7 @@ package net.sourceforge.html5val.examples;
 
 import javax.validation.constraints.*;
 
+import net.sourceforge.html5val.performers.URLPerformer;
 import org.hibernate.validator.constraints.*;
 
 import java.math.BigDecimal;
