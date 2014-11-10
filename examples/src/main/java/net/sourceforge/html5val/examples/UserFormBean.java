d2cb39bddb4b5bac434c1ef21d32d97d87c057a3
jacksonbeat <jacksonbeat>
2014-11-10 22:19:05 +0000

    @Pattern field added Updated version to 2.1.2-SNAPSHOT

--- examples/src/main/java/net/sourceforge/html5val/examples/UserFormBean.java
+++ examples/src/main/java/net/sourceforge/html5val/examples/UserFormBean.java
@@ -1,79 +1,92 @@
 package net.sourceforge.html5val.examples;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.URL;
+
 import javax.validation.constraints.*;
-
-import org.hibernate.validator.constraints.*;
-
 import java.math.BigDecimal;
 
 public class UserFormBean {
 
-    @Email
-    @NotEmpty
-    private String username;
+	@NotEmpty
+	@Email
+	private String username;
 
-    @Size(min = 5, max = 10)
-    private String code;
+	@Size(min = 5, max = 10)
+	private String code;
 
-    @Min(value = 18)
-    @Max(value = 100)
-    @NotNull
-    private Integer age;
+	@Min(value = 18)
+	@Max(value = 100)
+	@NotNull
+	private Integer age;
 
-    @Range(min = 0, max = 10)
-    private Integer highSchoolMark;
+	@Range(min = 0, max = 10)
+	private Integer highSchoolMark;
 
-    @URL
-    private String personalWebPage;
+	@URL
+	private String personalWebPage;
 
-    @Digits(integer = 3, fraction = 2)
-    private BigDecimal price;
+	@Digits(integer = 3, fraction = 2)
+	private BigDecimal price;
 
-    public String getUsername() {
-        return username;
-    }
+	@Pattern(regexp = "[0-9]{3}-[0-9]{6}")
+	private String phoneNumber;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public String getCode() {
-        return code;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+	public String getCode() {
+		return code;
+	}
 
-    public Integer getAge() {
-        return age;
-    }
+	public void setCode(String code) {
+		this.code = code;
+	}
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+	public Integer getAge() {
+		return age;
+	}
 
-    public Integer getHighSchoolMark() {
-        return highSchoolMark;
-    }
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
-    public void setHighSchoolMark(Integer highSchoolMark) {
-        this.highSchoolMark = highSchoolMark;
-    }
+	public Integer getHighSchoolMark() {
+		return highSchoolMark;
+	}
 
-    public String getPersonalWebPage() {
-        return personalWebPage;
-    }
+	public void setHighSchoolMark(Integer highSchoolMark) {
+		this.highSchoolMark = highSchoolMark;
+	}
 
-    public void setPersonalWebPage(String personalWebPage) {
-        this.personalWebPage = personalWebPage;
-    }
+	public String getPersonalWebPage() {
+		return personalWebPage;
+	}
 
-    public BigDecimal getPrice() {
-        return price;
-    }
+	public void setPersonalWebPage(String personalWebPage) {
+		this.personalWebPage = personalWebPage;
+	}
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 }
