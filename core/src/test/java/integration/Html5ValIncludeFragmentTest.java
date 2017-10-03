9a02aa27c472985f4c3358848daf4065a7b05bfd
masa.kunikata <masa.kunikata@gmail.com>
2017-10-03 16:31:50 +0900

    add th:include fragment test

---
+++ core/src/test/java/integration/Html5ValIncludeFragmentTest.java
@@ -0,0 +1,45 @@
+package integration;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.thymeleaf.context.Context;
+import org.w3c.dom.Document;
+
+import integration.bean.AllConstraintsBean;
+import integration.checker.HtmlChecker;
+
+public class Html5ValIncludeFragmentTest extends IntegrationTestBase {
+
+    private Context context = new Context();
+    private HtmlChecker checker;
+
+    @Before
+    public void setUp() {
+        context.setVariable("allConstraintsBean", new AllConstraintsBean());
+        Document html = processTemplate("fragments/mainForm.html", context);
+        checker = new HtmlChecker(html);
+    }
+
+
+    @Test
+    public void allConstraintsNameAttrForm() throws IOException {
+
+        checker.elementWithId("min10Max200Field").containsAttributeWithValue("type", "number");
+        checker.elementWithId("min10Max200Field").containsAttributeWithValue("max", "200");
+        checker.elementWithId("min10Max200Field").containsAttributeWithValue("min", "10");
+
+        checker.elementWithId("notNullField").containsAttributeWithValue("type", "text");
+        checker.elementWithId("notNullField").containsAttributeWithValue("required", "required");
+
+        checker.elementWithId("patternAlphabet5charsField").containsAttributeWithValue("type", "text");
+        checker.elementWithId("patternAlphabet5charsField").containsAttributeWithValue("pattern", "^[a-z]{5}$");
+
+        checker.elementWithId("size5to10Field").containsAttributeWithValue("type", "text");
+        checker.elementWithId("size5to10Field").containsAttributeWithValue("pattern", ".{5,10}");
+        checker.elementWithId("size5to10Field").containsAttributeWithValue("required", "required");
+        checker.elementWithId("size5to10Field").containsAttributeWithValue("maxlength", "10");
+    }
+
+}
