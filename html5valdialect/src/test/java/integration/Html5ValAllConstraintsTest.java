c581f3b1ba559cb839e6fdab41c038927d7a6787
masa.kunikata <masa.kunikata@gmail.com>
2017-10-04 10:37:46 +0900

    add th:field test

--- html5valdialect/src/test/java/integration/Html5ValAllConstraintsTest.java
+++ html5valdialect/src/test/java/integration/Html5ValAllConstraintsTest.java
@@ -2,7 +2,6 @@ package integration;
 
 import java.io.IOException;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.thymeleaf.context.Context;
 import org.w3c.dom.Document;
@@ -15,16 +14,18 @@ public class Html5ValAllConstraintsTest {
     private Context context = new Context();
     private HtmlChecker checker;
 
-    @Before
-    public void setUp() {
-        context.setVariable("allConstraintsBean", new AllConstraintsBean());
-        Document html = IntegrationTestUtils.processTemplate(IntegrationTestUtils.initDefaultTemplateEngine(),
-                "allConstraintsNameAttrForm.html", context);
-        checker = new HtmlChecker(html);
-    }
-
     @Test
     public void allConstraintsNameAttrForm() throws IOException {
+        //Arrange
+        context.setVariable("allConstraintsBean", new AllConstraintsBean());
+
+        //Act
+        Document html = IntegrationTestUtils.processTemplate(IntegrationTestUtils.initDefaultTemplateEngine(),
+                "allConstraintsNameAttrForm.html", context);
+
+        //Assert
+        checker = new HtmlChecker(html);
+
         checker.elementWithId("digitsInt3Frac2Field").containsAttributeWithValue("type", "text");
         checker.elementWithId("digitsInt3Frac2Field").containsAttributeWithValue("pattern",
                 "([0-9]{1,3}\\.?|\\.[0-9]{1,2}|[0-9]{1,3}\\.[0-9]{1,2}){1}");
@@ -75,4 +76,65 @@ public class Html5ValAllConstraintsTest {
                 "^http://localhost:8080(/.*)?");
     }
 
+    @Test
+    public void allConstraintsThFieldAttrForm() throws IOException {
+        //Arrange
+        context.setVariable("allConstraintsBean", new AllConstraintsBean());
+
+        //Act
+        Document html = IntegrationTestUtils.processTemplate(IntegrationTestUtils.initDefaultTemplateEngine(),
+                "allConstraintsThFieldAttrForm.html", context);
+
+        //Assert
+        checker = new HtmlChecker(html);
+
+        checker.elementWithId("digitsInt3Frac2Field").containsAttributeWithValue("type", "text");
+        checker.elementWithId("digitsInt3Frac2Field").containsAttributeWithValue("pattern",
+                "([0-9]{1,3}\\.?|\\.[0-9]{1,2}|[0-9]{1,3}\\.[0-9]{1,2}){1}");
+
+        checker.elementWithId("max100Field").containsAttributeWithValue("type", "number");
+        checker.elementWithId("max100Field").containsAttributeWithValue("max", "100");
+
+        checker.elementWithId("min18Field").containsAttributeWithValue("type", "number");
+        checker.elementWithId("min18Field").containsAttributeWithValue("min", "18");
+
+        checker.elementWithId("min10Max200Field").containsAttributeWithValue("type", "number");
+        checker.elementWithId("min10Max200Field").containsAttributeWithValue("max", "200");
+        checker.elementWithId("min10Max200Field").containsAttributeWithValue("min", "10");
+
+        checker.elementWithId("notNullField").containsAttributeWithValue("type", "text");
+        checker.elementWithId("notNullField").containsAttributeWithValue("required", "required");
+
+        checker.elementWithId("patternAlphabet5charsField").containsAttributeWithValue("type", "text");
+        checker.elementWithId("patternAlphabet5charsField").containsAttributeWithValue("pattern", "^[a-z]{5}$");
+
+        checker.elementWithId("size5to10Field").containsAttributeWithValue("type", "text");
+        checker.elementWithId("size5to10Field").containsAttributeWithValue("pattern", ".{5,10}");
+        checker.elementWithId("size5to10Field").containsAttributeWithValue("required", "required");
+        checker.elementWithId("size5to10Field").containsAttributeWithValue("maxlength", "10");
+
+        checker.elementWithId("emailField").containsAttributeWithValue("type", "email");
+
+        checker.elementWithId("length6to11Field").containsAttributeWithValue("type", "text");
+        checker.elementWithId("length6to11Field").containsAttributeWithValue("pattern", ".{6,11}");
+        checker.elementWithId("length6to11Field").containsAttributeWithValue("required", "required");
+        checker.elementWithId("length6to11Field").containsAttributeWithValue("maxlength", "11");
+
+        checker.elementWithId("notBlankField").containsAttributeWithValue("type", "text");
+        checker.elementWithId("notBlankField").containsAttributeWithValue("required", "required");
+
+        checker.elementWithId("notEmptyField").containsAttributeWithValue("type", "text");
+        checker.elementWithId("notEmptyField").containsAttributeWithValue("required", "required");
+
+        checker.elementWithId("rangeMin0Max10Field").containsAttributeWithValue("type", "range");
+        checker.elementWithId("rangeMin0Max10Field").containsAttributeWithValue("min", "0");
+        checker.elementWithId("rangeMin0Max10Field").containsAttributeWithValue("max", "10");
+
+        checker.elementWithId("urlField").containsAttributeWithValue("type", "url");
+        checker.elementWithId("urlField").containsAttributeWithValue("pattern", "^.+://.+(:[0-9]+)?(/.*)?");
+
+        checker.elementWithId("urlHttpLocalhost8080Field").containsAttributeWithValue("type", "url");
+        checker.elementWithId("urlHttpLocalhost8080Field").containsAttributeWithValue("pattern",
+                "^http://localhost:8080(/.*)?");
+    }
 }
